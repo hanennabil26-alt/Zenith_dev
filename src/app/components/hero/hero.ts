605b019f7e2110a9124518224067ae18281f40e6
hanennabil26-alt <hanen.nabil26@gmail.com>
2026-03-25 22:16:34 +0200

    force update zenith

--- src/app/components/hero/hero.ts
+++ src/app/components/hero/hero.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrl: './hero.scss'
 })
 export class HeroComponent {
-  heroBg = 'https://www.figma.com/api/mcp/asset/ce0823f8-f0f3-43a4-9e54-a3636c318124';
+  heroBg = '/building.jpg.png';
 }
